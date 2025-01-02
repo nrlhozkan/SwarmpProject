fed430d15b7c2df2561d0700fbcbf0faa4645155
nrlhozkan <ozkan.nrlh@gmail.com>
2025-01-02 11:14:09 +0100

    Added target_positions file location. I add 'to do' for users to change based on their folder paths.

--- Search and Resque/utils/config.py
+++ Search and Resque/utils/config.py
@@ -12,15 +12,18 @@ DISPLAY_NAME = 'Admin'
 # DATA STORAGE CONFIGURATION
 
 # this file is read by charlie to place the drones
-INITIAL_DRONE_POS_FILE = r'C:\Users\cgadmin\Desktop\charlie-mnemonic-dev\charlie-mnemonic-dev\Data\charlie_shared_data\initial_drone_pos.json' 
+INITIAL_DRONE_POS_FILE = r'C:\Users\cgadmin\Desktop\charlie-mnemonic-dev\charlie-mnemonic-dev\Data\charlie_shared_data\initial_drone_pos.json'  # to do: change this to the correct path
 
 # this is where charlie stores the positions (of one step)
-DRONE_POS_FILE = r'C:\Users\cgadmin\Desktop\charlie-mnemonic-dev\charlie-mnemonic-dev\Data\charlie_shared_data\drone_pos.json'  
+DRONE_POS_FILE = r'C:\Users\cgadmin\Desktop\charlie-mnemonic-dev\charlie-mnemonic-dev\Data\charlie_shared_data\drone_pos.json'   # to do: change this to the correct path
 
 # files and directories for collected information about experiments
-EXPERIMENT_BASE_FOLDER = r'C:\Users\cgadmin\Desktop\charlie\experiments'
+EXPERIMENT_BASE_FOLDER = r'C:\Users\cgadmin\Desktop\charlie\experiments' 
 # TODO this is where the experiment data will be stored
 #   for each time the script is executed all the experiment data will be stored in a new directory (see EXPERIMENT_FOLDER_FORMAT below)
+
+# Target positions are stored in this file
+TARGET_POS_FILE = r'C:\Users\cgadmin\Desktop\charlie-mnemonic-dev\charlie-mnemonic-dev\Data\other_data\target_positions.json' # to do: change this to the correct path
 
 EXPERIMENT_FOLDER_FORMAT = 'data_%d_%m_%Y_%H_%M_%S'
 
